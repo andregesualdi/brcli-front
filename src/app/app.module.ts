5611d608a632a4ca56d08f2b509c82d56d2dd9e5
andregesualdi <andrerges@gmail.com>
2023-09-19 21:22:51 -0300

    Commit Inicial: Estrutura base do front-end em Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BotaoComponent } from './core/components/mobile/botao/botao.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BotaoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/components/mobile/botao/botao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  templateUrl: './botao.component.html',
+  styleUrls: ['./botao.component.scss']
+})
+export class BotaoComponent {
+
+}
