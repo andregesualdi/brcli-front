39e55a76d1d7b48e5054fd2ccf6a0a0dcf51092a
andregesualdi <andrerges@gmail.com>
2023-09-24 20:01:38 -0300

    Criação da página de primeiro acesso e ajustes finos, finalização da feature de login

--- src/app/core/modules/mobile/pages/primeiro-acesso/primeiro-acesso.component.ts
+++ src/app/core/modules/mobile/pages/primeiro-acesso/primeiro-acesso.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-primeiro-acesso',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./primeiro-acesso.component.scss']
 })
 export class PrimeiroAcessoComponent {
+  public codigoValido: boolean = false;
+  public validarDesabilitado: boolean = true;
+  public cadastrarDesabilitado: boolean = true;
+  public codigoCadastro: string = '';
+  public loginUsuario: string = '';
+  public senhaUsuario: string = '';
 
+  constructor(
+    private router: Router
+  ) { }
+
+  public recuperarLogin(event: any): void {
+    this.loginUsuario = event;
+    this.validarCamposCadastro();
+  }
+
+  public recuperarSenha(event: any): void {
+    this.senhaUsuario = event;
+    this.validarCamposCadastro();
+  }
+
+  public recuperarCodigo(event: any): void {
+    this.codigoCadastro = event;
+    this.validarCampoCodigo();
+  }
+
+  public validarCodigo(): void {
+    this.codigoValido = true;
+  }
+
+  public cadastrar(): void {
+    this.router.navigate(['']);
+  }
+  
+  private validarCampoCodigo(): void {
+    if (this.codigoCadastro && this.codigoCadastro !== '') {
+      console.log(this.codigoCadastro);
+      this.validarDesabilitado = false;
+    } else {
+      this.validarDesabilitado = true;
+    }
+  }
+
+  private validarCamposCadastro(): void {
+    if (this.loginUsuario && this.loginUsuario !== ''
+      && this.senhaUsuario && this.senhaUsuario !== '') {
+      console.log(this.loginUsuario);
+      console.log(this.senhaUsuario);
+      this.cadastrarDesabilitado = false;
+    } else {
+      this.cadastrarDesabilitado = true;
+    }
+  }
 }
