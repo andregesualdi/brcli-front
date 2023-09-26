a6eb460eb84de21a085b6a7c59804a3e47f241b0
andregesualdi <andrerges@gmail.com>
2023-09-25 21:23:51 -0300

    Criação do serviço de dados do paciente e salvar imagem e integração mockada

--- src/app/core/services/dados.service.ts
+++ src/app/core/services/dados.service.ts
@@ -7,6 +7,8 @@ import { HttpHeaders } from '@angular/common/http';
 import { PlanoMetas } from '../shared/models/plano-metas.model';
 import { SalvarMetasResponse } from '../shared/models/salvar-metas-response.model';
 import { AvaliacaoFisica } from '../shared/models/avaliacao.model';
+import { Paciente } from '../shared/models/paciente.model';
+import { ImagemSalva } from '../shared/models/imagem-salva.model';
 
 @Injectable({
   providedIn: 'root'
@@ -48,5 +50,20 @@ export class DadosService {
     headers.append('x-usuario', usuario);
     return this.rest.get(environment.api.endpoints.avaliacaoFisica,
       headers);
+  }
+  
+  public recuperarDadosPaciente(usuario: string): Observable<Paciente> {
+    const headers = new HttpHeaders().set(
+      'Content-type', 'application/json'
+    );
+    headers.append('x-usuario', usuario);
+    return this.rest.get(environment.api.endpoints.dadosPaciente,
+      headers);
+  }
+
+  public salvarImagemPaciente(req: Paciente): Observable<ImagemSalva> {
+    return this.rest.put(environment.api.endpoints.alterarImagem,
+      req,
+      new HttpHeaders().set('Content-type', 'application/json'));
   }
 }
